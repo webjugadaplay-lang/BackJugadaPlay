b1d83f002a78cc815f8673ca19ab9e4381de3130
jugadaplay <webjugadaplay@gmail.com>
2026-06-25 02:00:45 -0300

    Vivo 2506-3

--- src/controllers/playerController.js
+++ src/controllers/playerController.js
@@ -134,9 +134,9 @@ const getLiveRoomData = async (req, res) => {
         entry_fee: room.entry_fee || 0,
         
         // Datos de equipos
-        team_home: room.Fixture?.team_home || 'Local',
+        team_home: fixture.home_team_name,
         home_team_logo: room.Fixture?.home_team_logo || '',
-        team_away: room.Fixture?.team_away || 'Visitante',
+        team_away: fixture.away_team_name,
         away_team_logo: room.Fixture?.away_team_logo || '',
         
         // Resultado final
